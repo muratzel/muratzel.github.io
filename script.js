4674b9ba070c3a47d8432a90fb06e45f2c138508
muratzel <ovmurad@colby.edu>
2016-01-28 15:56:27 -0500

    Second Test

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 Parse.initialize("EJ3swVy8iVnXKAO6XvT2LhGhYJ4BKLjFqRiuuxyX", "U5KZUB7IOm6JTwhdicpaBGxhVRtcJh2lOpHfH519");
 
-var TestObject = Parse.Object.extend("TestObject");
-var testObject = new TestObject();
-testObject.save({foo: "bar"}).then(function(object) {
-  alert("yay! it worked");
+var User = Parse.Object.Extend("User",{
+    
+    initialize: function(attrs,options,username,password){
+        this.username = username;
+        this.password = password;
+    },
+    
+    verifyPassword: function(password){
+        return this.password = password;
+    }
 });
+
+
