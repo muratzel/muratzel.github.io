3e1c38a455f449884b98b46f95990ee2a8e52eb2
muratzel <ovmurad@colby.edu>
2016-01-28 12:41:40 -0500

    Second Test

---
+++ script.js
@@ -0,0 +1,7 @@
+Parse.initialize("EJ3swVy8iVnXKAO6XvT2LhGhYJ4BKLjFqRiuuxyX", "U5KZUB7IOm6JTwhdicpaBGxhVRtcJh2lOpHfH519");
+
+var TestObject = Parse.Object.extend("TestObject");
+var testObject = new TestObject();
+testObject.save({foo: "bar"}).then(function(object) {
+  alert("yay! it worked");
+});
