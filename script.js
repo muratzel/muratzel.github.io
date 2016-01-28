384df27e447c89e75b50c30544c2a439e76ffbe6
muratzel <ovmurad@colby.edu>
2016-01-28 16:17:06 -0500

    Second Test

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 Parse.initialize("EJ3swVy8iVnXKAO6XvT2LhGhYJ4BKLjFqRiuuxyX", "U5KZUB7IOm6JTwhdicpaBGxhVRtcJh2lOpHfH519");
 
-// A complex subclass of Parse.Object
-var Monster = Parse.Object.extend("Monster", {
-  // Instance methods
-  hasSuperHumanStrength: function () {
-    return this.get("strength") > 18;
-  },
-  // Instance properties go in an initialize method
-  initialize: function (attrs, options) {
-    this.sound = "Rawr"
-  }
-},
-{}
-);
+class Monster extends Parse.Object {
+    constructor() {
+        // Pass the ClassName to the Parse.Object constructor
+        super('Monster');
+        // All other initialization
+        this.sound = 'Rawr';
+    }
+
+    hasSuperHumanStrength() {
+        return this.get('strength') > 18;
+    }
+
+    static spawn(strength) {
+        var monster = new Monster();
+        monster.set('strength', strength);
+        return monster;
+    }
+}
 
 var monster = Monster.spawn(200);
 alert(monster.get('strength'));  // Displays 200.
