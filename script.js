895647e4a9c5effae22b0388b2e5c4d77ee37a7d
muratzel <ovmurad@colby.edu>
2016-01-28 22:05:23 -0500

    commiting

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 Parse.initialize("EJ3swVy8iVnXKAO6XvT2LhGhYJ4BKLjFqRiuuxyX", "U5KZUB7IOm6JTwhdicpaBGxhVRtcJh2lOpHfH519");
 
 
-var loginForm = document.getElementById("loginForm");
-var signupForm = document.getElementById("signupForm");
-var mainDiv = document.getElementById("mainDiv");
+var loginForm = $("loginForm");
+var signupForm = $("signupForm");
+var mainDiv = $("mainDiv");
 
 var currentUser = new Parse.User.current();
 
 if (currentUser) {
-    loginForm.style.display = "none";
-    signupForm.style.display = "none";
-    mainDiv.style.display = "initial";
+    loginForm.addClass("hidden")
+    signupForm.addClass("hidden");
+    mainDiv.removeClass("hidden");
 }
 else {
-    loginForm.style.display = "initial";
-    signupForm.style.display = "none";
-    mainDiv.style.display = "none";
+    loginForm.removeClass("hidden");
+    signupForm.addClass("hidden");
+    mainDiv.addClass("hidden");
 }
 
 
