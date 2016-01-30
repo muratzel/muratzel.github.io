3f553c496d460d17ba7cdf2efab726abfc720286
muratzel <ovmurad@colby.edu>
2016-01-30 00:16:17 -0500

    Second Test

--- script.js
+++ script.js
@@ -91,7 +91,7 @@ backButton.click(function () {
 });
 addTagButton.click(function () {
     tagListDiv.append("<a href='#' class = 'list-group-item'>" + $('#addTagInput').val() + "</a>");
-    $('#addTagInput').innerHTML('');
+    $('#addTagInput').val('');
 })
 addTutorialButton.click(function () {
 
