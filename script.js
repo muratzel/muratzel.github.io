3584ebe0601149386b0e068c827e677ebdf5e598
muratzel <ovmurad@colby.edu>
2016-01-30 00:44:12 -0500

    Second Test

--- script.js
+++ script.js
@@ -90,7 +90,7 @@ backButton.click(function () {
     location.reload();
 });
 addTagButton.click(function () {
-    tagListDiv.append("<span class='label label-primary col-md-2'>" + $('#addTagInput').val() + "<button type='button' class='close'>&times;</button></span>");
+    tagListDiv.append("<span class='label label-primary col-md-2'>" + $('#addTagInput').val() + "<button type='button' class='close sm'>&times;</button></span>");
     $('#addTagInput').val('');
 })
 addTutorialButton.click(function () {
