ff2ac8f76303dc282e2c9684cea1db9cda1751aa
muratzel <ovmurad@colby.edu>
2016-01-29 21:29:00 -0500

    Second Test

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 Parse.initialize("EJ3swVy8iVnXKAO6XvT2LhGhYJ4BKLjFqRiuuxyX", "U5KZUB7IOm6JTwhdicpaBGxhVRtcJh2lOpHfH519");
 
-var user = Parse.User.current();
+var loginForm = $("#loginForm");
+var signupForm = $("#signupForm");
+var mainPageDiv = $("#mainPageDiv");
+
+function displayMainPage() {
+    loginForm.addClass("hidden");
+    signupForm.addClass("hidden");
+    mainPageDiv.removeClass("hidden");
+}
+function displayLoginForm() {
+    loginForm.removeClass("hidden");
+    signupForm.addClass("hidden");
+    mainPageDiv.addClass("hidden");
+}
+function displaySignupForm() {
+    loginForm.addClass("hidden");
+    signupForm.removeClass("hidden");
+    mainPageDiv.addClass("hidden");
+}
+
+var currentUser = Parse.User.current();
+
+if (currentUser) {
+    displayPage();
+}
+else {
+    displayLoginForm();
+}
 
 
 
