0da8749e47de9218e64bb8bbefdea331b2f31f70
muratzel <ovmurad@colby.edu>
2016-01-30 07:51:57 -0500

    lala

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ function populateWithTutorials() {
         {
             success: function (tutorials) {
                 for (var i = 0; i < tutorials.length; i++) {
-                    mainPageTutorialsDisplayDiv.append("<a class='tutorialA list-group-item' onclick = 'populateModal(`"+tutorials[i].id+"`);' data-toggle ='modal' data-target='#showTutorialModal' id='" + tutorials[i].id + "'><h3>" + tutorials[i].get('title') + "<small class='col-md-offset-1'>" + tutorials[i].get('type') + "</small></h3></a>");
+                    mainPageTutorialsDisplayDiv.append("<a class='tutorialA list-group-item' onclick = 'populateModal(this);' data-toggle ='modal' data-target='#showTutorialModal' id='" + tutorials[i].id + "'><h3>" + tutorials[i].get('title') + "<small class='col-md-offset-1'>" + tutorials[i].get('type') + "</small></h3></a>");
                 }
             },
             error: function (schedules, error) {
@@ -65,9 +65,9 @@ function populateWithTutorials() {
         }
     );
 }
-function populateModal(id) {
+function populateModal(tutorial) {
     var query = new Parse.Query(Tutorial);
-    query.get(id,
+    query.get(tutorial.attr("id"),
         {
             success: function (tutorial) {
                 tutorialTitleModalP.html(tutorial.get('title'));
@@ -160,7 +160,7 @@ addTutorialButton.click(function () {
     newTutorial.save(null,
         {
             success: function (tutorial) {
-                mainPageTutorialsDisplayDiv.append("<a href ='#' class='tutorialA list-group-item' onclick = 'populateModal(`" + tutorial.id + "`);' data-toggle ='modal' data-target='#showTutorialModal' id='" + tutorial.id + "'><h3>" + tutorial.get('title') + "<small class='col-md-offset-1'>" + tutorial.get('type') + "</small></h3></a>");
+                mainPageTutorialsDisplayDiv.append("<a href ='#' class='tutorialA list-group-item' onclick = 'populateModal(this);' data-toggle ='modal' data-target='#showTutorialModal' id='" + tutorial.id + "'><h3>" + tutorial.get('title') + "<small class='col-md-offset-1'>" + tutorial.get('type') + "</small></h3></a>");
             },
             error: function (tutorial, error) {
             }
